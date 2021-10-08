af66214bfd433d448e6cf9332e0f942c1a3fbfc9
James_Calvo <15jcalvo@gmail.com>
2021-10-08 13:19:10 -0400

    Task 4 array inside function

--- index.js
+++ index.js
@@ -243,9 +243,9 @@ Example born in 1901 and died in 1959 - included -- born in 1889 and died in 192
 If correct, the function should return ["Salvador Dali", "Frida Kahlo"]*/
 // Hint - Look up the .split() method
 // console.log(artists[0].years.split(' - '))
-const livedIn1900s = [];
+// const livedIn1900s = [];
 function get20s(array) {
-  // const livedIn1900s = [];
+  const livedIn1900s = [];
   for(let i = 0; i < array.length; i++){
     const splitYears = artists[i].years.split(' - ');
     // console.log(splitYears);
